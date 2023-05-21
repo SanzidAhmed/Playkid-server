606fe0ac4c4a8ef0fe6cc7ec80385d587d3b6aec
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-05-22 00:29:58 +0600

    post jobs and get alljobs

--- index.js
+++ index.js
@@ -26,12 +26,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toysCollection = client.db('toyDB').collection('toys')
+    console.log('database connected successfully');
+
+    app.post('/postjobs', async(req, res) => {
+      const body = req.body;
+      console.log(body);
+      const result = await toysCollection.insertOne(body);
+      res.send(result);
+      console.log(body);
+    })
+    app.get('/alljobs', async(req, res) => {
+      const result = await toysCollection.find({}).toArray();
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
